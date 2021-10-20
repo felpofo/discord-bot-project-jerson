8c48a62ac491eb08c864fc0b139aae061b8509ba
felpofo <felipepitolpuhl@gmail.com>
2021-10-20 11:23:42 -0300

    src

--- @types/readline/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "readline" {
-  interface Interface {
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    commands: { [key: string]: Function };
-    clearLine(): void;
-  }
-}
